cea098ee7c883cee4fd3e436572e424c0bf998a1
NattapolChan <66078943+NattapolChan@users.noreply.github.com>
2021-09-18 17:02:18 +0700

    To note

--- Audiotonote.py
+++ Audiotonote.py
@@ -46,7 +46,6 @@ def fft_plot(audio, sampling_rate, start_sec, stop_sec):
   print(peaks)
   return y, x, peaks
 
-
 y, x, peaks = fft_plot(sample, sampling_rate, 20.00, 20.1)
 print(y.shape)
 print(x.shape)
@@ -55,7 +54,6 @@ print(len(peaks))
 for i in range(len(peaks)):
   print(x[peaks[i]])
   print(y[int(x[peaks[i]])])
-
 
 freq_wave = {}
 for second in range(math.floor(10*len(sample)/ sampling_rate)):
@@ -67,5 +65,32 @@ for second in range(math.floor(10*len(sample)/ sampling_rate)):
     print(y[int(x[peaks[i]])])
     freq_wave[second].append(x[peaks[i]])
 
-
 print(freq_wave)
+
+req_note = [82.41,87.31,92.50,98.00,103.83,110.00,116.54,123.47,130.81,138.59,146.83,155.56,164.81,174.61,185.00,196.00,207.65,220.00,233.08,246.94,110.00,116.54,123.47,130.81,138.59,146.83,155.56,164.81,174.61,185.00,196.00,207.65,220.00,233.08,246.94,261.63,277.18,293.66,311.13,329.63,146.83,155.56,164.81,174.61,185.00,196.00,207.65,220.00,233.08,246.94,261.63,277.18,293.66,311.13,329.63,349.23,369.99,392.00,415.30,440.00,196.00,207.65,220.00,233.08,246.94,261.63,277.18,293.66,311.13,329.63,349.23,369.99,392.00,415.30,440.00,466.16,493.88,523.25,554.37,587.33,246.94,261.63,277.18,293.66,311.13,329.63,349.23,369.99,392.00,415.30,440.00,466.16,493.88,523.25,554.37,587.33,622.25,659.26,698.46,739.99,329.63,349.23,369.99,392.00,415.30,440.00,466.16,493.88,523.25,554.37,587.33,622.25,659.26,698.46,739.99,783.99,830.61,880.00,932.33 ,987.77]
+print(len(freq_note))
+def FindNote(fre):
+  index = 19
+  for i in range(len(freq_note)):
+    if abs(freq_note[i]-fre) < abs(freq_note[index]-fre):
+      index = i
+    if abs(freq_note[i]-fre) == abs(freq_note[index]-fre) and i%20 < index%20:
+      index = i
+      #print(abs(fre - freq_note[index]))
+  #print("====================")
+  string_num = ['E','A','D','G','B','e']
+  string_name = string_num[math.floor(index/20)]
+  string_name += str(round(index%20))
+  return string_name
+
+#!------------ change freq_note to dict ------------!#
+#!-------- or create preset frequency range --------!#
+note_wave = {}
+for second in range(math.floor(10*len(sample)/ sampling_rate)):
+  second = second/10
+  arr = freq_wave[second]
+  note_array = []
+  for j in range(len(arr)):
+    note_array.append(FindNote(arr[j]))
+  note_wave[second] = note_array
+print(note_wave)
