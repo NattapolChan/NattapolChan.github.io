c622cfb48ff7cdfe3c6494182c8daec88923ced2
NattapolChan <66078943+NattapolChan@users.noreply.github.com>
2021-09-19 19:51:55 +0700

    App

---
+++ Tabframe.js
@@ -0,0 +1,12 @@
+function setup(){
+    createCanvas(windonWidth, windowHeight)
+}
+function preload(){
+    url = "C:\Users\6200307\Desktop\webweb\TabGenerator\tabjson.json" //add request and reciever
+    tabs = loadJSON(url)
+    console.log(tabs)
+}
+
+function draw(){
+
+}
