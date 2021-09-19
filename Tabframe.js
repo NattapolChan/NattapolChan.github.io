100058f09bd72e0805db5705df7e30d7b882a969
NattapolChan <66078943+NattapolChan@users.noreply.github.com>
2021-09-19 21:28:34 +0700

    structure

--- Tabframe.js
+++ Tabframe.js
@@ -1,12 +1,27 @@
 function setup(){
-    createCanvas(windonWidth, windowHeight)
+    createCanvas(windowWidth, windowHeight)
+    lineinterval = height/60
 }
 function preload(){
-    url = "C:\Users\6200307\Desktop\webweb\TabGenerator\tabjson.json" //add request and reciever
+    url = "tabjson.json" //add request and reciever
     tabs = loadJSON(url)
-    console.log(tabs)
 }
 
-function draw(){
+var start = true
+$(document).ready(function() {
+	const bclick = document.getElementById('playandpause');
+	bclick.addEventListener('click', function red () {
+        start = !start
+	});
+});
 
+function draw(){
+    stroke(100)
+    displayfret(height/7)
+}
+
+function displayfret(y){
+    for(i=0;i<6;i++){
+        line(width/10,y+i*lineinterval, 9*width/10, y+ i*lineinterval)
+    }
 }
