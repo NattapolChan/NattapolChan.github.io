d7b1dd6e421e20bc5fd2c17b05849c747f6c5250
NattapolChan <66078943+NattapolChan@users.noreply.github.com>
2021-09-20 22:26:20 +0700

    Update Tabframe.js

--- Tabframe.js
+++ Tabframe.js
@@ -1,6 +1,8 @@
 function setup(){
     createCanvas(windowWidth * 0.85, windowHeight)
-    lineinterval = height/60
+    winheight = height
+    winwidth = width
+    lineinterval = winheight/60
 }
 function preload(){
     url = "tabjson.json" //add request and reciever
@@ -18,14 +20,45 @@ $(document).ready(function() {
 var numberofline = 7
 function draw(){
     stroke(100)
-    displayfret(height/7)
     for(j=1;j<numberofline+1;j++){
-        displayfret(j*height/7)
+        displayfret(j*winheight/7)
+        displaytime(j*winheight/7)
     }
+    drawnote(winheight/7)
 }
 
 function displayfret(y){
     for(i=0;i<6;i++){
-        line(width/10,y+i*lineinterval, 9*width/10, y+ i*lineinterval)
+        stroke(100)
+        line(winwidth/10,y+i*lineinterval, 9*winwidth/10, y+ i*lineinterval)
+    }
+}
+
+function displaytime(y){
+    for(i=1;i<6;i++){
+        stroke(200)
+        line(i*winwidth/6, y-lineinterval, i*winwidth/6, 6*lineinterval + y)
+    }
+}
+
+function drawnote(y){
+    for(i=0;i<40;i++){
+        sec = i/10
+        sec = str(sec.toFixed(1))
+        numberofnote = tabs[sec].length
+        for(j=0;j<numberofnote;j++){
+            displaynote( tabs[sec][j], i/10, y)
+        }
+    }
+}
+
+function displaynote(note, xposition, y){
+    guistring = ['E','A','D','G','B','e']
+    xpos = map(xposition, 0, 4, winwidth/6, 5*winwidth/6)
+    for(m=0;m<6;m++){
+        if(note[0] == guistring[m]){
+            fill(100, 100, 100)
+            text(note[1] , xpos, (5.5-m)*lineinterval + y)
+        }
     }
 }
